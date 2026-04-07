4c11586c7766d0546f4e3e0a6aad8b08ffc1846d
Damian Puister <damian@letsgetdigital.com>
2026-04-07 14:55:27 +0200

    chore: remove SQLite migrations, regenerate Prisma client for PostgreSQL

--- prisma/migrations/20260402171137_add_knowledge_entries/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "KnowledgeEntry" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "category" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "source" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- prisma/migrations/20260407075558_add_claude_token_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "claudeEmail" TEXT;
-ALTER TABLE "User" ADD COLUMN "claudeRefreshToken" TEXT;
-ALTER TABLE "User" ADD COLUMN "claudeToken" TEXT;
-ALTER TABLE "User" ADD COLUMN "claudeTokenExpiresAt" DATETIME;
